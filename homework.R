69637fdae4005da9f0d7afeb88b3419227a7b4fb
lauraashlock <lashl003@ucr.edu>
2022-01-19 16:49:42 -0800

    completed assignment 1

--- homework.R
+++ homework.R
@@ -1,7 +1,8 @@
-#PSYC 259 Homework 1 - Data Import
+#PSYC 259 Homework 1 - Data Import - Laura Ashlock 
 #For full credit, provide answers for at least 6/8 questions
 
 #List names of students collaborating with (no more than 2): 
+#Merab Gomez 
 
 #GENERAL INFO 
 #data_A contains 12 files of data. 
@@ -19,6 +20,7 @@
 
 # ANSWER
 
+library(readr)
 
 ### QUESTION 2 ----- 
 
@@ -45,8 +47,35 @@
 col_names  <-  c("trial_num","speed_actual","speed_response","correct")
 
 # ANSWER
+fname <- "data_A/6191_1.txt"
+colname <- c("trial_num", "speed_actual", "speed_response", "correct")
+coltypes<- "nccc"
+ds1 <- read_delim(file = fname, delim = "\t", col_names = colname, col_types = coltypes, skip=7)
+print(ds1)
 
-
+# A tibble: 20 × 4
+#trial_num speed_actual speed_response correct
+#<dbl> <chr>        <chr>          <chr>  
+#1         1 fas          slower         False  
+#2         2 fas          faster         True   
+#3         3 fas          faster         True   
+#4         4 fas          slower         False  
+#5         5 fas          faster         True   
+#6         6 slo          slower         True   
+#7         7 fas          faster         True   
+#8         8 slo          slower         True   
+#9         9 slo          slower         True   
+#10        10 slo          faster         False  
+#11        11 slo          slower         True   
+#12        12 fas          faster         True   
+#13        13 slo          slower         True   
+#14        14 fas          faster         True   
+#15        15 slo          faster         False  
+#16        16 fas          faster         True   
+#17        17 slo          slower         True   
+#18        18 slo          slower         True   
+#19        19 slo          slower         True   
+#20        20 fas          faster         True 
 
 ### QUESTION 3 ----- 
 
@@ -55,7 +84,9 @@ col_names  <-  c("trial_num","speed_actual","speed_response","correct")
 # Then write the new data to a CSV file in the "data_cleaned" folder
 
 # ANSWER
-
+ds1$trialnum_new <- ds1$trial_num+99
+#used point and click to add data_cleaned folder... not sure if there is code?
+write_csv(ds1, file = "data_cleaned/trials_relabeled.csv")
 
 ### QUESTION 4 ----- 
 
@@ -63,14 +94,23 @@ col_names  <-  c("trial_num","speed_actual","speed_response","correct")
 # Store it to a variable
 
 # ANSWER
+data_A_files<- list.files('data_A', full.names = TRUE)
 
+# Make a variable containing the list of data files
+full_file_names <- list.files('data_raw', full.names = TRUE)
+# Pass the list to read_csv to read all of them into a single tibble
+ds_all <- read_csv(full_file_names)
+print(ds_all)
 
 ### QUESTION 5 ----- 
 
 # Read all of the files in data_A into a single tibble called ds
 
 # ANSWER
-
+colname <- c("trial_num", "speed_actual", "speed_response", "correct")
+coltypes<- "iccl"
+ds <- read_tsv(data_A_files, col_names = colname, col_types = coltypes, skip=7)
+print(ds)
 
 ### QUESTION 6 -----
 
@@ -83,7 +123,9 @@ col_names  <-  c("trial_num","speed_actual","speed_response","correct")
 # (It should work now, but you'll see a warning because of the erroneous data point)
 
 # ANSWER
-
+coltypes<- "iccl"
+col_types=coltypes
+ds$trialnum_new <- ds$trial_num+99
 
 ### QUESTION 7 -----
 
@@ -93,7 +135,11 @@ col_names  <-  c("trial_num","speed_actual","speed_response","correct")
 # Re-import the data so that filename becomes a column
 
 # ANSWER
-
+# ANSWER
+colname <- c("trial_num", "speed_actual", "speed_response", "correct")
+coltypes<- "iccl"
+ds <- read_tsv(data_A_files, col_names = colname, col_types = coltypes, skip=7, id="block_num")
+print(ds)
 
 ### QUESTION 8 -----
 
@@ -102,4 +148,14 @@ col_names  <-  c("trial_num","speed_actual","speed_response","correct")
 # There are two sheets of data -- import each one into a new tibble
 
 # ANSWER
+install.packages("readxl")
+library(readxl)
+read_xlsx("data_B/participant_info.xlsx")
+excel_sheets("data_B/participant_info.xlsx")
+ds_participants<- read_xlsx("data_B/participant_info.xlsx", sheet = "participant")
+print(ds_participants)
 
+colname <- c("participant", "test_date")
+coltypes<- c("numeric", "date")
+ds_testdate<- read_xlsx("data_B/participant_info.xlsx", sheet = "testdate", col_names = colname, col_types = coltypes)
+print(ds_testdate)
